a6ab431088ffc52102b2e103359300fc6e4bd48a
Lee-Orr <leeorr@gmail.com>
2025-07-31 17:21:32 -0400

    import road trips

--- import-road-trip.ts
+++ import-road-trip.ts
@@ -1,5 +1,6 @@
 import roadTrips from "./road-trips.json" with { type: "json" };
 import TurndownService from "npm:turndown";
+import { dateToString } from "https://deno.land/x/date_format_deno/mod.ts";
 
 const service = new TurndownService();
 
@@ -7,11 +8,15 @@ async function convert() {
   await Promise.all(
     roadTrips.map(async (v) => {
       const md = service.turndown(v.Description);
+      const date = new Date(v["Date + Time"]);
+      const formattedDate = dateToString("yyyy-MM-dd", date);
       const mdoc = `---
 title: "${v.Name}"
 city: "${v.City}"
 state: "${v.State}"
-
+date: "${formattedDate}"
+organizationUrl: "${v["Organization URL"]}"
+details: "${v["Event details URL"]}"
 ---
 
 ${md}`;
