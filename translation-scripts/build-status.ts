d51a1a89f4f78f9a939f3dee350d52e3bb7ad31a
andybarrandy <52949512+andybarrandy@users.noreply.github.com>
2025-10-27 21:18:36 -0700

    Add formatDate to build-status

--- translation-scripts/build-status.ts
+++ translation-scripts/build-status.ts
@@ -9,6 +9,23 @@ const Id = process.env.BUILD_CONTROLLER_ID;
 const Status = process.env.BUILD_CONTROLLER_STATUS;
 const url = clientConfig.baseUrl + "/api/v2/tables/mr0t0b8iilyhqq7/records";
 
+function formatDate(date: Date): string {
+  return (
+    date
+      .toLocaleString("en-US", {
+        timeZone: "America/Los_Angeles",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+      })
+      .replace(",", "") + " PT"
+  );
+}
+
 async function triggerUpdate() {
   if (!Id || !Status) {
     console.error("Cant update controller", Id, Status);
@@ -27,7 +44,7 @@ async function triggerUpdate() {
     body: JSON.stringify([
       {
         Id: Number.parseInt(Id),
-        Status: `${Status} - ${new Date().toISOString()}`,
+        Status: `${Status} - ${formatDate(new Date())}`,
       },
     ]),
   });
